7e1843aff222ab5b0dd7c1d4e4a833101f6ccfea
moniquetrotta <monique.trotta@gmail.com>
2022-09-16 11:31:45 -0300

    inlcusão de api viacep e utilização do axios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
+import api from './services/api';
 
 function App() {
   const [input, setInput] = useState('');
+  const [cep, setCEP] = useState('');
 
-  function handleSearch() {
-    alert('Valor do Input' + input);
+  async function handleSearch() {
+    //22080050/json/
+    if (input === '') {
+      alert('Preencha algum cep');
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json/`);
+      setCEP(response.data);
+      setInput('');
+    } catch {
+      alert('ops! Erro ao buscar aqui');
+      setInput('');
+    }
+    //alert('Valor do Input' + input);
   }
   return (
     <div className="container">
@@ -24,13 +39,16 @@ function App() {
         </button>
       </div>
 
-      <main className="main">
-        <h2>CEP: </h2>
-        <span>Rua</span>
-        <span>Complemento</span>
-        <span>Bairro</span>
-        <span>Estado</span>
-      </main>
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP:{cep.cep}</h2>
+          <span>Rua: {cep.logradouro}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
     </div>
   );
 }
