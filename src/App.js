298c5b542a82fe7cdcead0258a6e87d5c880deb9
moniquetrotta <monique.trotta@gmail.com>
2022-09-16 10:05:27 -0300

    inclusão de React Icons no projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { FiSearch } from 'react-icons/fi';
+
 function App() {
   return (
-    <div className="App">
-      <h1>teste projeto</h1>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input type="text" placeholder="Digite o CEP" />
+        <button className="buttonSearch">
+          <FiSearch size={25} color="FFF" />
+        </button>
+      </div>
     </div>
   );
 }
